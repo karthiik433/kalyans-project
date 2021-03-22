b73682f39435f3fb5f5fa70b04ef7b8e3fe957a3
karthiik433 <kartheek12345kumar@gmail.com>
2021-03-22 15:53:22 +0530

    intial upload

---
+++ project/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+//import {imageUrl} from "./assests";
+//const url = "./images/studio.png"
+
+function App() {
+  return (
+   <>
+   <h1>karthiii</h1>
+   <h2>achaa sare sare lee voyy</h2>
+  </>
+  );
+}
+
+export default App;
